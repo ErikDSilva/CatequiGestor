3077748c6c2773841f7e8ed8beb664163ceeea78
Erik Silva <oliveiraerik464@gmail.com>
2025-04-13 22:16:19 -0300

    first commit

---
+++ src/main/java/joseph/catechesis/CatequiGestor/CatequiGestorApplication.java
@@ -0,0 +1,13 @@
+package joseph.catechesis.CatequiGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatequiGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatequiGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joseph/catechesis/CatequiGestor/CatequiGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package joseph.catechesis.CatequiGestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatequiGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
